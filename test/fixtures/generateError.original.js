11ad2dfa595675a95d0dfba23109bf1ff7cc0125
Thomas Watson <w@tson.dk>
2017-03-30 20:52:39 +0200

    More test coverage

--- test/fixtures/generateError.original.js
+++
@@ -1,4 +0,0 @@
-// Just a little prefixing line
-const generateError = (errMessage = 'Some error') => new Error(errMessage)
-
-module.exports = generateError
--- test/sourcemap.js
+++ test/sourcemap.js
@@ -2,53 +2,149 @@
 
 var path = require('path')
 var test = require('tape')
-var generateError = require('./fixtures/generateError')
 var stackman = require('../')()
 
-test('sourcemapped location getters', function (t) {
-  var err = generateError()
-  stackman.callsites(err, function (err, callsites) {
-    t.error(err)
-    var callsite = callsites[0]
-    t.equal(callsite.getFileName(), path.join(__dirname, 'fixtures', 'generateError.original.js'))
-    t.equal(callsite.getLineNumber(), 2)
-    t.equal(callsite.getColumnNumber(), 53)
-    t.end()
+test('source maps disabled', function (t) {
+  var err = require('./fixtures/lib/error')()
+  stackman.callsites(err, {sourcemap: false}, assertSourceMapDisabled.bind(null, t))
+})
+
+test('source map inlined', function (t) {
+  var err = require('./fixtures/lib/error-inline')()
+  stackman.callsites(err, assertSourceFound.bind(null, t))
+})
+
+test('source map linked', function (t) {
+  t.test('source mapped source code embedded', function (t) {
+    var err = require('./fixtures/lib/error-src-embedded')()
+    stackman.callsites(err, assertSourceFound.bind(null, t))
+  })
+
+  t.test('source mapped source code on disk', function (t) {
+    var err = require('./fixtures/lib/error')()
+    stackman.callsites(err, assertSourceFound.bind(null, t))
+  })
+
+  t.test('source mapped source code not found', function (t) {
+    var err = require('./fixtures/lib/error-src-missing')()
+    stackman.callsites(err, assertSourceNotFound.bind(null, t))
   })
 })
 
-test('sourcemapped location getters fall back if no sourcemap exists', function (t) {
-  stackman.callsites(new Error(), function (err, callsites) {
-    t.error(err)
-    var callsite = callsites[0]
-    t.equal(callsite.getFileName(), __filename)
-    t.notEqual(callsite.getLineNumber(), 2)
-    t.notEqual(callsite.getColumnNumber(), 53)
-    t.end()
-  })
-})
+test('fails', function (t) {
+  t.test('inlined source map broken', function (t) {
+    var err = require('./fixtures/lib/error-inline-broken')()
+    stackman.callsites(err, function (err, callsites) {
+      t.ok(err.message.indexOf('Error parsing sourcemap') === 0)
+      var callsite = callsites[0]
+      t.equal(callsite.getFileName(), path.join(__dirname, 'fixtures', 'lib', 'error-inline-broken.js'))
+      t.equal(callsite.getRelativeFileName(), path.join('test', 'fixtures', 'lib', 'error-inline-broken.js'))
+      t.equal(callsite.getLineNumber(), 6)
+      t.equal(callsite.getColumnNumber(), 10)
+      t.equal(callsite.getFunctionName(), 'generateError')
+      t.equal(callsite.getFunctionNameSanitized(), 'generateError')
+      t.equal(callsite.isApp(), __dirname.indexOf('node_modules') === -1)
 
-test('sourcemapped context', function (t) {
-  var err = generateError()
-  stackman.callsites(err, function (err, callsites) {
-    t.error(err)
-    callsites[0].sourceContext(function (err, context) {
-      t.error(err)
-      t.deepEqual(context, {
-        pre: ['// Just a little prefixing line'],
-        line: 'const generateError = (errMessage = \'Some error\') => new Error(errMessage)',
-        post: ['', 'module.exports = generateError', '']
+      callsite.sourceContext(function (err, context) {
+        t.error(err)
+        t.equal(context.line, '  return new Error(msg);')
+        t.end()
       })
-      t.end()
+    })
+  })
+
+  t.test('linked source map not found', function (t) {
+    var err = require('./fixtures/lib/error-map-missing')()
+    stackman.callsites(err, function (err, callsites) {
+      t.equal(err.code, 'ENOENT')
+      var callsite = callsites[0]
+      t.equal(callsite.getFileName(), path.join(__dirname, 'fixtures', 'lib', 'error-map-missing.js'))
+      t.equal(callsite.getRelativeFileName(), path.join('test', 'fixtures', 'lib', 'error-map-missing.js'))
+      t.equal(callsite.getLineNumber(), 6)
+      t.equal(callsite.getColumnNumber(), 10)
+      t.equal(callsite.getFunctionName(), 'generateError')
+      t.equal(callsite.getFunctionNameSanitized(), 'generateError')
+      t.equal(callsite.isApp(), __dirname.indexOf('node_modules') === -1)
+
+      callsite.sourceContext(function (err, context) {
+        t.error(err)
+        t.equal(context.line, '  return new Error(msg);')
+        t.end()
+      })
+    })
+  })
+
+  t.test('linked source map broken', function (t) {
+    var err = require('./fixtures/lib/error-broken')()
+    stackman.callsites(err, function (err, callsites) {
+      t.error(err)
+      var callsite = callsites[0]
+      t.equal(callsite.getFileName(), path.join(__dirname, 'fixtures', 'lib', 'error-broken.js'))
+      t.equal(callsite.getRelativeFileName(), path.join('test', 'fixtures', 'lib', 'error-broken.js'))
+      t.equal(callsite.getLineNumber(), 6)
+      t.equal(callsite.getColumnNumber(), 10)
+      t.equal(callsite.getFunctionName(), 'generateError')
+      t.equal(callsite.getFunctionNameSanitized(), 'generateError')
+      t.equal(callsite.isApp(), __dirname.indexOf('node_modules') === -1)
+
+      callsite.sourceContext(function (err, context) {
+        t.error(err)
+        t.equal(context.line, '  return new Error(msg);')
+        t.end()
+      })
     })
   })
 })
 
-test('callsite.getRelativeFileName()', function (t) {
-  var err = generateError()
-  stackman.callsites(err, function (err, callsites) {
+function assertSourceMapDisabled (t, err, callsites) {
+  t.error(err)
+  var callsite = callsites[0]
+  t.equal(callsite.getFileName(), path.join(__dirname, 'fixtures', 'lib', 'error.js'))
+  t.equal(callsite.getRelativeFileName(), path.join('test', 'fixtures', 'lib', 'error.js'))
+  t.equal(callsite.getLineNumber(), 6)
+  t.equal(callsite.getColumnNumber(), 10)
+  t.equal(callsite.getFunctionName(), 'generateError')
+  t.equal(callsite.getFunctionNameSanitized(), 'generateError')
+  t.equal(callsite.isApp(), __dirname.indexOf('node_modules') === -1)
+  callsite.sourceContext(function (err, context) {
     t.error(err)
-    t.equal(callsites[0].getRelativeFileName(), 'test/fixtures/generateError.original.js')
+    t.equal(context.line, '  return new Error(msg);')
     t.end()
   })
-})
+}
+
+function assertSourceFound (t, err, callsites) {
+  t.error(err)
+  var callsite = callsites[0]
+  t.equal(callsite.getFileName(), path.join(__dirname, 'fixtures', 'src', 'error.js'))
+  t.equal(callsite.getRelativeFileName(), path.join('test', 'fixtures', 'src', 'error.js'))
+  t.equal(callsite.getLineNumber(), 2)
+  t.equal(callsite.getColumnNumber(), 39)
+  t.equal(callsite.getFunctionName(), 'generateError')
+  t.equal(callsite.getFunctionNameSanitized(), 'generateError')
+  t.equal(callsite.isApp(), __dirname.indexOf('node_modules') === -1)
+  callsite.sourceContext(function (err, context) {
+    t.error(err)
+    t.deepEqual(context.pre, ['// Just a little prefixing line'])
+    t.equal(context.line, 'const generateError = (msg = \'foo\') => new Error(msg)')
+    t.deepEqual(context.post, ['', 'module.exports = generateError', ''])
+    t.end()
+  })
+}
+
+function assertSourceNotFound (t, err, callsites) {
+  t.error(err)
+  var callsite = callsites[0]
+  t.equal(callsite.getFileName(), path.join(__dirname, 'fixtures', 'src', 'not', 'found.js'))
+  t.equal(callsite.getRelativeFileName(), path.join('test', 'fixtures', 'src', 'not', 'found.js'))
+  t.equal(callsite.getLineNumber(), 2)
+  t.equal(callsite.getColumnNumber(), 39)
+  t.equal(callsite.getFunctionName(), 'generateError')
+  t.equal(callsite.getFunctionNameSanitized(), 'generateError')
+  t.equal(callsite.isApp(), __dirname.indexOf('node_modules') === -1)
+  callsite.sourceContext(function (err, context) {
+    t.equal(err.code, 'ENOENT')
+    t.equal(context, undefined)
+    t.end()
+  })
+}
